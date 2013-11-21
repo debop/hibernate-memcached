2c203989937c10f1b6fb8bf41b83ab48129bea45
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2013-11-21 17:16:43 +0900

    -. implement AbstractMemcachedRegionFactory

--- src/main/java/org/hibernate/cache/memcached/AbstractMemcachedRegionFactory.java
+++ src/main/java/org/hibernate/cache/memcached/AbstractMemcachedRegionFactory.java
@@ -1,69 +1,108 @@
 package org.hibernate.cache.memcached;
 
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.cache.CacheException;
+import org.hibernate.cache.memcached.client.HibernateMemcached;
+import org.hibernate.cache.memcached.regions.*;
+import org.hibernate.cache.memcached.strategy.MemcachedAccessStrategyFactory;
+import org.hibernate.cache.memcached.strategy.MemcachedAccessStrategyFactoryImpl;
 import org.hibernate.cache.spi.*;
 import org.hibernate.cache.spi.access.AccessType;
 import org.hibernate.cfg.Settings;
 
+import java.util.HashSet;
 import java.util.Properties;
+import java.util.Set;
 
 /**
- * org.hibernate.cache.memcached.AbstractMemcachedRegionFactory
+ * Memcached Region Factory
  *
  * @author 배성혁 sunghyouk.bae@gmail.com
  * @since 2013. 11. 20. 오전 9:22
  */
-public class AbstractMemcachedRegionFactory implements RegionFactory {
+@Slf4j
+abstract public class AbstractMemcachedRegionFactory implements RegionFactory {
 
+    public static final String MEMCACHED_CACHE_CONFIGURATION_RESOURCE_NAME = "memcached.cache.configurationResourceName";
 
-    @Override
-    public void start(Settings settings, Properties properties) throws CacheException {
+    protected Settings settings;
+    protected final Properties props;
+    protected final MemcachedAccessStrategyFactory accessStrategyFactory = new MemcachedAccessStrategyFactoryImpl();
+    protected final Set<String> regionNames = new HashSet<String>();
+    protected HibernateMemcached memcached = null;
 
+    public AbstractMemcachedRegionFactory(Properties props) {
+        this.props = props;
     }
 
-    @Override
-    public void stop() {
-
-    }
-
+    /**
+     * Whether to optimize for minimals puts or minimal gets.
+     * <p/>
+     * Indicates whether when operating in non-strict read/write or read-only mode
+     * Hibernate should optimize the access patterns for minimal puts or minimal gets.
+     * In Ehcache we default to minimal puts since this should have minimal to no
+     * affect on unclustered users, and has great benefit for clustered users.
+     * <p/>
+     * This setting can be overridden by setting the "hibernate.cache.use_minimal_puts"
+     * property in the Hibernate configuration.
+     *
+     * @return true, optimize for minimal puts
+     */
     @Override
     public boolean isMinimalPutsEnabledByDefault() {
-        return false;
+        return true;
     }
 
     @Override
     public AccessType getDefaultAccessType() {
-        return null;
+        return AccessType.READ_WRITE;
     }
 
     @Override
     public long nextTimestamp() {
-        return 0;
+        return System.currentTimeMillis() / 100;
     }
 
     @Override
-    public EntityRegion buildEntityRegion(String s, Properties properties, CacheDataDescription cacheDataDescription) throws CacheException {
-        return null;
+    public EntityRegion buildEntityRegion(String regionName,
+                                          Properties properties,
+                                          CacheDataDescription metadata) throws CacheException {
+        regionNames.add(regionName);
+        return new MemcachedEntityRegion(accessStrategyFactory, memcached, regionName, settings, metadata, properties);
     }
 
     @Override
-    public NaturalIdRegion buildNaturalIdRegion(String s, Properties properties, CacheDataDescription cacheDataDescription) throws CacheException {
-        return null;
+    public NaturalIdRegion buildNaturalIdRegion(String regionName,
+                                                Properties properties,
+                                                CacheDataDescription metadata) throws CacheException {
+        regionNames.add(regionName);
+        return new MemcachedNaturalIdRegion(accessStrategyFactory, memcached, regionName, settings, metadata, properties);
     }
 
     @Override
-    public CollectionRegion buildCollectionRegion(String s, Properties properties, CacheDataDescription cacheDataDescription) throws CacheException {
-        return null;
+    public CollectionRegion buildCollectionRegion(String regionName,
+                                                  Properties properties,
+                                                  CacheDataDescription metadata) throws CacheException {
+        regionNames.add(regionName);
+        return new MemcachedCollectionRegion(accessStrategyFactory, memcached, regionName, settings, metadata, properties);
     }
 
     @Override
-    public QueryResultsRegion buildQueryResultsRegion(String s, Properties properties) throws CacheException {
-        return null;
+    public QueryResultsRegion buildQueryResultsRegion(String regionName, Properties properties) throws CacheException {
+        regionNames.add(regionName);
+        return new MemcachedQueryResultsRegion(accessStrategyFactory,
+                                               memcached,
+                                               regionName,
+                                               properties);
     }
 
     @Override
-    public TimestampsRegion buildTimestampsRegion(String s, Properties properties) throws CacheException {
-        return null;
+    public TimestampsRegion buildTimestampsRegion(String regionName, Properties properties) throws CacheException {
+        regionNames.add(regionName);
+        return new MemcachedTimestampsRegion(accessStrategyFactory,
+                                             memcached,
+                                             regionName,
+                                             properties);
     }
 
     private static final long serialVersionUID = -4083654729815324429L;
